000f073016fd4c90a282abbebbea9c80f8830400
i-Ahmed <i-ahmed-r@gmail.com>
2024-04-23 12:38:23 +0300

    update-commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://idevil1.github.io/rest-countries-with-react-and-redux/",
 });
